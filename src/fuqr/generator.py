73309d19bb7eb5c14fc7b33498b5fbb7aba5ecae
futa-t <143526236+futa-t@users.noreply.github.com>
2024-10-11 22:35:39 +0900

    復数行対応とエラー処理の追加

--- src/fuqr/generator.py
+++ src/fuqr/generator.py
@@ -2,6 +2,7 @@ import tkinter
 
 import qrcode
 from PIL import Image, ImageTk
+from qrcode.exceptions import DataOverflowError
 
 from fuqr import util
 
@@ -20,20 +21,48 @@ class QrGenerator:
         self.lbl_qr = tkinter.Label(self._root, text="文字列を入力してください")
         self.lbl_qr.pack(side=tkinter.TOP)
         self._size_qr = (300, 300)
-        self._value = tkinter.StringVar(value=value)
-        tkinter.Entry(self._root, textvariable=self._value).pack(fill=tkinter.X)
-        self._value.trace_add("write", self.regen)
-        if value:
-            self.generate_qrcode(value)
 
-    def regen(self, *args):
-        self.generate_qrcode(self._value.get())
+        self.txt_value = tkinter.Text(
+            self._root, relief=tkinter.FLAT, height=1, width=-1, font=("", 12)
+        )
+
+        self.txt_value.bind("<<Modified>>", self._get_value)
+        self.txt_value.bind("<KeyRelease>", self.on_txt_keyrelease)
+        self.txt_value.pack(fill=tkinter.X)
+
+        self.txt_value.focus()
+
+        self._msg = tkinter.StringVar()
+        lbl_status = tkinter.Label(self._root, textvariable=self._msg)
+        lbl_status.pack(fill=tkinter.X, side=tkinter.BOTTOM)
+
+    def on_txt_keyrelease(self, e: tkinter.Event):
+        value = self.txt_value.get("1.0", "end-1c")
+        self.txt_value["height"] = value.count("\n") + 1
+
+    def _get_value(self, e):
+        self._value = self.txt_value.get("1.0", "end-1c")
+        self.generate_qrcode(self._value)
+        self.txt_value.edit_modified(False)
 
     def generate_qrcode(self, value):
-        im_qr = qrcode.make(value)
+        try:
+            if value == "":
+                self.lbl_qr["image"] = ""  # Clear the current image
+                raise ValueError
+            im_qr = qrcode.make(value)
+            self._im_qr = ImageTk.PhotoImage(im_qr.resize(self._size_qr, Image.LANCZOS))
+            self.lbl_qr["image"] = self._im_qr
+            self._msg.set("作成成功")
+        except DataOverflowError:
+            self._show_error("データが長すぎます。短い文字列を入力してください。")
+        except ValueError:
+            self._show_error("無効な入力です。")
+        except Exception as e:
+            self._show_error(f"予期せぬエラーが発生しました: {str(e)}")
 
-        self._im_qr = ImageTk.PhotoImage(im_qr.resize(self._size_qr, Image.LANCZOS))
-        self.lbl_qr["image"] = self._im_qr
+    def _show_error(self, message):
+        self._msg.set(message)
 
     def run(self):
         match type(self._root):
